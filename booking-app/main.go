63ce639a979d96fafbfab9dbb0179665dfc53ba2
sekharmisra <59437658+sekharmisra@users.noreply.github.com>
2023-08-23 07:37:05 -0400

    Committed changes for Slices chapter! in golang crash course

--- booking-app/main.go
+++ booking-app/main.go
@@ -6,7 +6,8 @@ func main() {
 	var conferenceName = "Go conference"
 	const totalTickets uint = 50
 	var remainingTickets uint = 50
-	var bookings [50]string
+	//var bookings []string
+	bookings := []string{}
 
 	fmt.Printf("Conference name is of type %T, totaltickets is of type %T, remainingtickets is of type %T \n", conferenceName, totalTickets, remainingTickets)
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
@@ -32,14 +33,15 @@ func main() {
 	fmt.Scan(&email)
 
 	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
 
-	bookings[0] = firstName + " " + lastName
-
-	fmt.Printf("The whole array %v\n", bookings)
-	fmt.Printf("The first element of the array %v\n", bookings[0])
-	fmt.Printf("The size of the array %v\n", len(bookings))
-	fmt.Printf("The type of the array %T\n", bookings)
+	fmt.Printf("The whole slice is: %v\n", bookings)
+	fmt.Printf("The first element of the slice is: %v\n", bookings[0])
+	fmt.Printf("The size of the slice is: %v\n", len(bookings))
+	fmt.Printf("The type of the slice %T\n", bookings)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive confirmation email on %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("Remaining tickets %v for the %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("All the bookings in the application: %v", bookings)
 }
