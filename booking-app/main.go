53c1ec47cf022d321cea8b557fa5422661541a13
sekharmisra <59437658+sekharmisra@users.noreply.github.com>
2023-08-24 21:06:17 -0400

    committed infinite for loop for booking application!

--- booking-app/main.go
+++ booking-app/main.go
@@ -15,33 +15,38 @@ func main() {
 	fmt.Println("Get your tickets here to attend")
 	fmt.Printf("Booking is of type  %T\n", bookings)
 
-	var firstName string
-	var lastName string
-	var userTickets uint
-	var email string
+	for {
+		//Infinite loop
+		var firstName string
+		var lastName string
+		var userTickets uint
+		var email string
 
-	fmt.Println("Enter your first name:")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name:")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter number of tickets to purchased:")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter number of tickets to purchased:")
+		fmt.Scan(&userTickets)
 
-	fmt.Println("Enter your email:")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email:")
+		fmt.Scan(&email)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("The whole slice is: %v\n", bookings)
-	fmt.Printf("The first element of the slice is: %v\n", bookings[0])
-	fmt.Printf("The size of the slice is: %v\n", len(bookings))
-	fmt.Printf("The type of the slice %T\n", bookings)
+		fmt.Printf("The whole slice is: %v\n", bookings)
+		fmt.Printf("The first element of the slice is: %v\n", bookings[0])
+		fmt.Printf("The size of the slice is: %v\n", len(bookings))
+		fmt.Printf("The type of the slice %T\n", bookings)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive confirmation email on %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("Remaining tickets %v for the %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive confirmation email on %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("Remaining tickets %v for the %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("All the bookings in the application: %v", bookings)
+		fmt.Printf("All the bookings in the application: %v\n", bookings)
+
+	}
+
 }
