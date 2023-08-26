5b627fe1e3211f0bfd583381e0ce4a9eb392230c
sekharmisra <59437658+sekharmisra@users.noreply.github.com>
2023-08-26 17:01:46 -0400

    Commited helper.go demonstrating packages in Go!

---
+++ booking-app/helper.go
@@ -0,0 +1,12 @@
+package main
+
+import "strings"
+
+// Validates user input
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidName, isValidEmail, isValidTicketNumber
+}
--- booking-app/main.go
+++ booking-app/main.go
@@ -82,15 +82,6 @@ func getFirstNames() []string {
 	return firstNames
 }
 
-// Validates user input
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-
-	return isValidName, isValidEmail, isValidTicketNumber
-}
-
 // Get user input
 func getUserInput() (string, string, string, uint) {
 	var firstName string
